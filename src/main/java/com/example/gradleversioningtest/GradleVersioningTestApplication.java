2b24ecd1d719f2957e056587b02d80f5d6ef1cc5
dev-sincerity <kllliv50@gmail.com>
2023-07-05 08:26:04 +0900

    fix: 안녕하세요 출력

--- src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
+++ src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradleVersioningTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("안녕하세요");
         SpringApplication.run(GradleVersioningTestApplication.class, args);
     }
 

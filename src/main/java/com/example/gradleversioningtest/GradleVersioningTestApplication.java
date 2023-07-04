d9b2b6c7b6c8f993cc0f3fe1a29d35b2f89868a7
dev-sincerity <kllliv50@gmail.com>
2023-07-05 08:14:11 +0900

    Initial commit

---
+++ src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradleversioningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleVersioningTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleVersioningTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradleversioningtest/GradleVersioningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradleversioningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleVersioningTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

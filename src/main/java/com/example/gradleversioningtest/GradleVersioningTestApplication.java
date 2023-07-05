d5e1081ed9e130c4b51a9b782994ab60f1ae2655
dev-sincerity <kllliv50@gmail.com>
2023-07-05 09:51:14 +0900

    fix: 1234

--- src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
+++ src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradleVersioningTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("안녕하세요557788");
+        System.out.println("안녕하세요99");
         SpringApplication.run(GradleVersioningTestApplication.class, args);
     }
 

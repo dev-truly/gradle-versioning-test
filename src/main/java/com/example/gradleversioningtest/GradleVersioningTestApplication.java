5c0f540915ce9f890c624b0aec3ad3cfd6fa6a57
dev-sincerity <kllliv50@gmail.com>
2023-07-05 09:13:52 +0900

    fix: 222

--- src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
+++ src/main/java/com/example/gradleversioningtest/GradleVersioningTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradleVersioningTestApplication {
 
     public static void main(String[] args) {
-        System.out.println("안녕하세요5");
+        System.out.println("안녕하세요4");
         SpringApplication.run(GradleVersioningTestApplication.class, args);
     }
 
